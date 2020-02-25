025e4c423fe1745d65fde9e0e807c8ee481573fd
Maritxis <henar.mendiola@gmail.com>
2020-02-25 05:51:05 +0100

    [master] getting initial posts from bbdd

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../components/Header'
 import Body from '../components/Body'
 import Footer from '../components/Footer'
 
 const Home = () => {
   const [step, setStep] = useState(1);
+  const [posts, setPosts] = useState([])
   const handleGoHome = () => setStep(1);
   const handleNext = () => setStep(step +1);
   const handleShare = () => {
     setStep(1);
   }
+
+  const getPosts = async () => {
+    const config = {
+      method: 'get',
+      url: 'http://localhost:3000/api/posts'
+    };
+    const res = await axios(config);
+    setPosts(res.data);
+    console.log(res.data);
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return(
     <>
       <h2>Home</h2>
