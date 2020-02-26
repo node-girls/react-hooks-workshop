b48e50f36277e6cf90f7793ed992de1aa14ebf88
Sonia Ruiz <yune.vk@gmail.com>
2020-02-26 20:10:05 +0100

    Add style

--- src/components/CardPosts.jsx
+++ src/components/CardPosts.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const CardPosts = ({post, handleLikes}) => (
-  <>
+const CardPosts = ({ post, handleLikes }) => (
+  <article>
     <div>
-      <img src={post.userImage} alt={post.username}/>
+      <img src={post.userImage} alt={post.username} />
       <p>{post.username}</p>
     </div>
     <div>
-      <img src={post.postImage} alt=""/>
+      <img src={post.postImage} alt="" />
       <div>
-        <button onClick={() => handleLikes(post)}> 
+        <button onClick={() => handleLikes(post)}>
           <i className="far fa-heart fa-lg"></i>
         </button>
         <p>{post.likes}</p>
         <p>{post.caption}</p>
       </div>
     </div>
-  </>
+  </article>
 )
 
 export default CardPosts;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Header = ({step, handleGoHome, handleShare, handleNext}) => (
-  <>
-    {step === 1 && <button onClick={handleGoHome}>Home</button>}
-    {(step === 2 || step === 3) && <button onClick={handleGoHome}>Cancel</button>}
-    {(step === 1 || step === 2) && <button onClick={handleNext}>Next</button>}
-    {step === 3 && <button onClick={handleShare}>Share</button>}
-  </>
+const Header = ({ step, handleGoHome, handleShare, handleNext }) => (
+  <header>
+    {step === 1 && <button onClick={handleGoHome}><img src="/img/nodegirls.svg" className="icon logo" alt="Home" /></button>}
+    {(step === 2 || step === 3) && <button onClick={handleGoHome}><img src="/img/nodegirls.svg" className="icon logo" alt="Home" /></button>}
+    {(step === 1 || step === 2) && <button onClick={handleNext}><img src="/img/right-arrow.svg" className="icon" alt="Siguiente" /></button>}
+    {step === 3 && <button onClick={handleShare}><img src="/img/share.svg" className="icon" alt="Enviar" /></button>}
+  </header>
 );
 
 export default Header;
